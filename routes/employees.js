0320211d197624bb7d461f9ba9842339346f1f76
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-23 20:23:08 -0400

    add: all AC met. moveing to review and readme update. plus bonus

--- routes/employees.js
+++ routes/employees.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Router = require('express').Router;
-const {allEmp,allDepartment,allRole, addEmp,updateEmp,addRole,titleOfRole} = require('../lib/prepSQL.js');
+const {allEmp,allDepartment,allRole, addEmp,updateEmp,addRole,titleOfRole,addDepartment} = require('../lib/prepSQL.js');
 const employees = Router();
 
 // employees.use(express.urlencoded({ extended: false }));
@@ -128,6 +128,37 @@ employees.get('/departments', async (req, res) => {
   //return results
   return res;
   
+});
+
+employees.post('/departments',async(req,res) =>{
+  // Query database  
+  // res.status(200).send(JSON.parse(await allEmp()));
+  // Destructuring assignment for the items in req.body
+  const { name } = req.body;
+  
+  // const requestedTerm = req.params.term.toLowerCase();
+  // console.info(first_name);
+  // console.info(last_name);
+  // console.info(role_id);
+  // console.info(manager_id);
+
+  // If all the required properties are present
+  if (name) {
+    // db stuff here
+    await addDepartment(name);
+
+    const response = {
+      status: 'success',
+      body: req.body,
+    };
+
+    // console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in adding department');
+  }
+  //return results
+  return res;
 });
 
 
