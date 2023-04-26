07279443a9e6e4716232eaeac7e375589c34e7a1
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-26 00:18:20 -0400

    add: all bonus completed, double check on our delete

--- routes/employees.js
+++ routes/employees.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Router = require('express').Router;
-const {allEmp, addEmp,updateEmp,viewAllEmp,managerLimit} = require('../lib/prepSQL.js');
+const {allEmp, addEmp,updateEmp,viewAllEmp,managerLimit, vbymanager, vbydepartment} = require('../lib/prepSQL.js');
 const employees = Router();
 
 // GET request for employees
@@ -15,7 +15,10 @@ employees.get('/employees', async (req, res) => {
 // GET request for employees/view
 employees.get('/employees/view', async (req, res) => {  
   // Query database  S
-  res.status(200).send(JSON.parse(await viewAllEmp()));
+ 
+    res.status(200).send(JSON.parse(await viewAllEmp()));
+  
+  
   
   //return results
   return res;
@@ -60,6 +63,22 @@ employees.post('/employees', async (req, res) => {
   }
   //return results
   return res;
+});
+employees.post('/employees/view:id:type', async (req,res)=> {
+  const opthatsdf = req.params;
+  let someData
+  const {id, type} = req.body;
+if (type === 'manager') {
+    someData = await vbymanager(id);
+    res.status(201).send(someData);
+  }
+  if (type === 'department') {
+    someData = await vbydepartment(id);
+    res.status(201).send(someData);
+  }
+  
+return res;
+
 });
 //PUT for updates
 employees.put('/employees/:id', async (req, res) => {   
