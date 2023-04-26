142f35aaa099223ec18190d3777ae35f9e48ef10
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-26 00:30:59 -0400

    add: more data for testing

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,6 +13,8 @@ VALUES
     (002,"Salesperson","80000",'1'),
     (003,"Lead Engineer","150000",'2'),
     (004,"Software Engineer","120000",'2'),
+    (010,"Jr. Software Engineer","100000",'2'),
+    (009,"Entry Software Engineer","85000",'2'),
     (005,"Account Manager","160000",'4'),
     (006,"Accountant","125000",'4'),
     (007,"Legal Team Lead","250000",'3'),
@@ -27,4 +29,12 @@ VALUES
     (005,"Kunal","Singh",'5',NULL),
     (006,"Malia","Brown",'6','5'),
     (007,"Sarah","Lourd",'7',NULL),
-    (008,"Tom","Allen",'8','7');
+    (008,"Tom","Allen",'8','7'),
+    (009,"Joe","Allen",'4','3'),
+    (010,"Lyle","Joseph",'10','3'),
+    (011,"Brain","Smith",'10','3'),
+    (012,"Bryan","Smith",'9','4'),
+    (013,"Kyle","Owl",'9','4'),
+    (014,"Heather","South",'9','9'),
+    (015,"Taylor","Beer",'9','9');
+    
